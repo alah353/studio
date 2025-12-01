fbc58d3f5b72f82bbc21aaccd37efc4fa7fc1d28
Ali Ahrika Ahrika <alah353@vidalibarraquer.net>
2025-12-01 16:47:19 +0000

    haz el logo mas grande,

--- src/components/layout/horse-logo.tsx
+++ src/components/layout/horse-logo.tsx
@@ -6,8 +6,8 @@ export function HorseLogo(props: { className?: string }) {
     <Image
       src="/logocaballo.png"
       alt="Horse S.L. Logo"
-      width={32}
-      height={32}
+      width={40}
+      height={40}
       className={props.className}
     />
   );
