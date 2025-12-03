8947f32320bb94065661b450c71a1358b7876a2c
Ali Ahrika Ahrika <alah353@vidalibarraquer.net>
2025-12-03 19:03:41 +0000

    ▲ Next.js 15.3.3    - Environments: .env
       Creating an optimized produ

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
+  // Your other configuration, if any
 };
 
 export default nextConfig;
